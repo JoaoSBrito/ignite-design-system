cadd1ffacae4023377cd430fca5f84cbef0c246f
joao vitor <joaovbrito123@gmail.com>
2023-05-12 15:18:41 -0300

    fix: trying to fix deploy

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,25 +1,22 @@
-/** @type { import('@storybook/react-vite').StorybookConfig } */
-const config = {
-  stories: ["../src/pages/**/*.mdx", "../src/stories/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-a11y"
-  ],
-  framework: {
+module.exports = {
+  "stories": ["../src/pages/**/*.stories.mdx", "../src/stories/**/*.stories.tsx"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-a11y", "@storybook/addon-mdx-gfm"],
+  "framework": {
     name: "@storybook/react-vite",
-    options: {},
+    options: {}
+  },
+  "features": {
+    "storyStoreV7": true
+  },
+  viteFinal: (config, {
+    configType
+  }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/05-design-system/';
+    }
+    return config;
   },
   docs: {
-    autodocs: "tag",
-  },
-  viteFinal: (config, {configType} ) => {
-    if (configType === 'PRODUCTION') {
-      config.base = '/ignite-design-system/'
-    }
-
-    return config
-  },
+    autodocs: true
+  }
 };
-export default config;
